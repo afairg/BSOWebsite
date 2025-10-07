6b117276bb74aaa0f8c4b91bb75e774b70dc16c9
Avery <averymrich22@gmail.com>
2025-10-07 00:31:26 -0500

    add hero-image component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BsoNavBar } from "../components/bso-nav-bar/bso-nav-bar";
+import { HeroImage } from "../components/hero-image/hero-image";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BsoNavBar],
+  imports: [RouterOutlet, BsoNavBar, HeroImage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/components/hero-image/hero-image.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-image',
+  imports: [],
+  templateUrl: './hero-image.html',
+  styleUrl: './hero-image.css'
+})
+export class HeroImage {
+
+}
